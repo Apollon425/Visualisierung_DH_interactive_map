684c8cfdd02d48c77cae76b37f46ae5591b54910
Apollon425 <richard.apollon@web.de>
2022-07-03 04:18:51 +0200

    added shapefile layer bezirke

--- app/script.js
+++ app/script.js
@@ -1,7 +1,11 @@
 var map = L.map('map').setView([51.34, 12.36], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 13,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
+
+//L.geoJson(data_bezirke).addTo(map);
+var myLayer = L.geoJSON().addTo(map);
+myLayer.addData(data_bezirke);
 
